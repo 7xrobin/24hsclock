97a51d7f7dbea575f123a3a3ce184246bf540a0d
robindesu <carlosrobon0@gmail.com>
2016-10-02 21:00:33 -0300

    Numbers

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
-(function(){
+var centerX = 500;
+var centerY = 240;
+var rHour = 190;
+var rMin = 150;
+var rSec = 120;
+var numbMargin= 10;
 
-    var writeNumbers;
 
-    writeNumbers =  function(){
-      for(var i=0; i<=24;i++){
-        $('.clockArc.hour').after(i);
-      }
-    };
-
-}).call(this);
-
-(function() {
-  var describeArc, polarToCartesian, setCaptions;
-
-  polarToCartesian = function(centerX, centerY, radius, angleInDegrees) {
+var polarToCartesian = function(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians;
     angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
     return {
@@ -21,6 +14,43 @@
       y: centerY + radius * Math.sin(angleInRadians)
     };
   };
+
+(function(){
+    var writeNumbers;
+    
+    writeNumbers =  function(){
+      var hourNumbers= $('#hourNumbers');
+      var minNumbers= $('#minNumbers');
+      var svgNS = "http://www.w3.org/2000/svg";
+      for(var i=0; i<24;i++){
+         var hourDegree= (i * 15);
+         var pos = polarToCartesian(centerX-numbMargin,centerY+numbMargin, rHour+30, hourDegree);
+         var newText = document.createElementNS(svgNS,"text");
+         newText.setAttributeNS(null,"x",pos.x.toString());      
+         newText.setAttributeNS(null,"y",pos.y.toString());   
+         newText.setAttributeNS(null,"id",i.toString());   
+         var textNode = document.createTextNode(i.toString());
+         newText.appendChild(textNode);
+         hourNumbers.append(newText);
+      }
+      for(var i=0; i<60;i+=5){
+         var minDegree=  i / 60 * 360;
+         var pos = polarToCartesian(centerX-7,centerY+7, rMin+20, minDegree);
+         var newText = document.createElementNS(svgNS,"text");
+         newText.setAttributeNS(null,"x",pos.x.toString());      
+         newText.setAttributeNS(null,"y",pos.y.toString());   
+         newText.setAttributeNS(null,"id",i.toString());   
+         var textNode = document.createTextNode(i.toString());
+         newText.appendChild(textNode);
+         minNumbers.append(newText);
+      }
+    };
+
+    writeNumbers();
+}).call(this);
+
+(function() {
+  var describeArc, setOrbits;
 
   describeArc = function(x, y, radius, startAngle, endAngle) {
     var arcSweep, end, start;
@@ -30,7 +60,7 @@
     return ['M', start.x, start.y, 'A', radius, radius, 0, arcSweep, 0, end.x, end.y].join(' ');
   };
 
-  setCaptions = function() {
+  setOrbits = function() {
     var dot, hour, hourArc, minArc, minute, now, pos;
     now = new Date();
     hour = now.getHours();
@@ -39,29 +69,29 @@
     hourArc = (hour * 60 + minute) / (24 * 60) * 360;
     minArc = minute / 60 * 360;
     secArc = second / 60 * 360;
-    $('.clockArc.hour').attr('d', describeArc(500, 240, 190, 0, hourArc));
-    $('.clockArc.minute').attr('d', describeArc(500, 240, 150, 0, minArc));
-    $('.clockArc.second').attr('d', describeArc(500, 240, 120, 0, secArc));
-    $('.clockDot.hour').attr('d', describeArc(500, 240, 190, hourArc - 1 , hourArc));
-    $('.clockDot.minute').attr('d', describeArc(500, 240, 150, minArc - 3, minArc));
+    $('.clockArc.hour').attr('d', describeArc(centerX, centerY, rHour, 0, hourArc));
+    $('.clockArc.minute').attr('d', describeArc(centerX, centerY, rMin, 0, minArc));
+    $('.clockArc.second').attr('d', describeArc(centerX, centerY, rSec, 0, secArc));
+    $('.clockDot.hour').attr('d', describeArc(centerX, centerY, rHour, hourArc , hourArc));
+    $('.clockDot.minute').attr('d', describeArc(centerX, centerY, rMin, minArc , minArc));
     dot = $(".clockDot.hour");
-    pos = polarToCartesian(500, 240, 190, hourArc);
+    pos = polarToCartesian(centerX, centerY, rHour, hourArc);
     dot.attr("cx", pos.x);
     dot.attr("cy", pos.y);
     dot = $(".clockDot.minute");
-    pos = polarToCartesian(500, 240, 150, minArc);
+    pos = polarToCartesian(centerX, centerY, rMin, minArc);
     dot.attr("cx", pos.x);
     dot.attr("cy", pos.y);
     dot = $(".clockDot.second");
-    pos = polarToCartesian(500, 240, 120, secArc);
+    pos = polarToCartesian(centerX, centerY, rSec, secArc);
     dot.attr("cx", pos.x);
     dot.attr("cy", pos.y);
   };
 
  
-  setCaptions();
+  setOrbits();
 
   setInterval(function() {
-    return setCaptions();
-  }, 1 * 1000);
+    return setOrbits();
+  }, 100);
 }).call(this);
